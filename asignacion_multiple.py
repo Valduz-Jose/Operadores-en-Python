ad48c9b6739f35b44fcde7822f476f83cfcda945
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-11 15:25:17 -0400

    Agregar script para demostrar asignación múltiple y encadenada en Python

---
+++ asignacion_multiple.py
@@ -0,0 +1,9 @@
+# Asignacion multiple
+
+x,y,z = 10,'Hola',-9.15
+print(f"El valor de x es: {x} y el valor de y es: {y} y el valor de z es: {z}")
+
+# Asignacion encadenada 
+# se usa el mismo valor para varias variables
+a = b = c = 20
+print(f"El valor de a es: {a} y el valor de b es: {b} y el valor de c es: {c}")
