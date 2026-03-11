6eae02263c58d65e3cc981197a874878e2eec0d9
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-11 15:22:05 -0400

    Agregar script para demostrar operadores de asignación en Python

---
+++ op_asignacion.py
@@ -0,0 +1,8 @@
+print("-----Operadores de asignacion-----")
+numero = 10
+print("El numero es: ",numero)
+numero = 5
+print("El numero es: ",numero)
+cadena = "Saludos desde python"
+print("La cadena es: ",cadena)
+
