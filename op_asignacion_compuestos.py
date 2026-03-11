da9601e6e88828361b12a5c586f47544bb8d7e14
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-11 15:51:49 -0400

    Agregar script para demostrar operadores de asignación compuestos en Python

---
+++ op_asignacion_compuestos.py
@@ -0,0 +1,18 @@
+# Operadores de Asignacion Compuestos 
+# Los operadores de asignacion compuestos son una forma abreviada de escribir una operacion aritmetica seguida de una asignacion.
+# += -= *= /= //= **= %=
+
+a,b = 10,15
+print(f"El valor de a es: {a} y el valor de b es: {b}")
+
+a += b # a = a + b
+print(f"El valor de a es: {a} y el valor de b es: {b}")
+
+a -= b # a = a - b
+print(f"El valor de a es: {a} y el valor de b es: {b}")
+
+a *= b # a = a * b
+print(f"El valor de a es: {a} y el valor de b es: {b}")
+
+a /= b # a = a / b
+print(f"El valor de a es: {a} y el valor de b es: {b}")
