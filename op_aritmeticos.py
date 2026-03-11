1289883fd36e0da3e633fe928a2ed83cd37484ea
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-11 15:20:06 -0400

    Agregar operaciones aritméticas básicas en Python

---
+++ op_aritmeticos.py
@@ -0,0 +1,30 @@
+# suma + ,resta -,multiplicacion *,division /,division entera //,modulo %,exponente **
+a = 10 
+b = 3
+
+#  suma
+suma = a+b
+print("La suma es: ",suma)
+# resta
+resta = a-b
+print("La resta es: ",resta)
+
+# multiplicacion
+multiplicacion = a*b
+print("La multiplicacion es: ",multiplicacion)
+
+# division
+division = a/b
+print("La division es: ",f"{division:.2f}")
+
+# division_entera
+division_entera = a//b
+print("La division_entera es: ",division_entera)
+
+# modulo
+modulo = a%b
+print("La modulo es: ",modulo)
+
+# exponente
+exponente = a**b
+print("La exponente es: ",exponente)
