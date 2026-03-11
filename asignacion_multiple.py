75c18911508e2e58567f57958ab517d7c763c658
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-11 15:29:39 -0400

    Agregar ejemplos de intercambio de valores y recepción de múltiples entradas en Python

--- asignacion_multiple.py
+++ asignacion_multiple.py
@@ -7,3 +7,13 @@ print(f"El valor de x es: {x} y el valor de y es: {y} y el valor de z es: {z}")
 # se usa el mismo valor para varias variables
 a = b = c = 20
 print(f"El valor de a es: {a} y el valor de b es: {b} y el valor de c es: {c}")
+
+# Intercambio de valores de una variable sin utilizar una variable temporal
+x,y = 5,10
+# Aplicando el concepto de asignacion multiple
+x,y = y,x
+print(f"El valor de x es: {x} y el valor de y es: {y}")
+
+# recibir multiples valores de entrada del usuario
+nombre,apellido = input("Ingrese su nombre y apellido separados por una coma: ").split(',')
+print(f"El nombre es: {nombre.strip()} y el apellido es: {apellido.strip()}")
