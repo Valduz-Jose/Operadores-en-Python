a798261b5f5bc02676166b2f1f60fe856b926853
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-12 09:54:21 -0400

    Agregar script para demostrar operadores de comparación en Python

---
+++ op_comparacion.py
@@ -0,0 +1,11 @@
+# Operadores comparacion o relacionales
+# == != < > <= >=
+a,b = 7,5
+print(f"Valor Inicial a: {a} b: {b}")
+
+print(f"a == b: {a == b}")
+print(f"a != b: {a != b}")
+print(f"a < b: {a < b}")
+print(f"a > b: {a > b}")
+print(f"a <= b: {a <= b}")
+print(f"a >= b: {a >= b}")
