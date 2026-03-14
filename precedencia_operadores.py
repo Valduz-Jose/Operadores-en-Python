64c0b4fbd2b1d8deb3f7a55895592379a8e2cfe5
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-14 10:55:39 -0400

    Agregar script para demostrar la precedencia de operadores en Python

---
+++ precedencia_operadores.py
@@ -0,0 +1,14 @@
+# la precedencia de operadores determina el orden en el que se evaluan las expresiones en una operación matemática o lógica. En Python, la precedencia de operadores sigue un conjunto de reglas que dictan el orden en el que se evalúan los operadores en una expresión.
+# A continuación se muestra una lista de los operadores en Python, ordenados por precedencia (de mayor a menor):
+# Parentesis () tiene la mayor precedencia
+# exponente **
+# unario +,-
+# multiplicación *, división /, módulo %, división entera //
+# suma +, resta -
+# operadores de comparación ==, !=, >, <, >=, <=
+# operadores lógicos and, or, not
+# Asignación =, +=, -=, *=, /=, %=, //=, **=
+
+# Ejemplo
+resultado = 12 / 3 +2 * 3 -1
+print(resultado)
